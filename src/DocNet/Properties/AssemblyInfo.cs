80ddf6374ed6c60399b6f12340c1b322b6199464
Frans Bouma <frans@sd.nl>
2017-08-21 11:01:45 +0200

    Updated assembly info for version

--- src/DocNet/Properties/AssemblyInfo.cs
+++ src/DocNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Docnet")]
-[assembly: AssemblyCopyright("Copyright ©2016 Frans Bouma")]
+[assembly: AssemblyCopyright("Copyright ©2017 Frans Bouma")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.0.0")]
-[assembly: AssemblyFileVersion("0.16.0")]
+[assembly: AssemblyVersion("0.16.1.0")]
+[assembly: AssemblyFileVersion("0.16.1")]
