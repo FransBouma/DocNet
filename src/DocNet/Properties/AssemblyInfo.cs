e989c065754a526beedf0ae8973ad076f4527048
Frans Bouma <frans@sd.nl>
2020-01-02 10:49:34 +0100

    Version bump

--- src/DocNet/Properties/AssemblyInfo.cs
+++ src/DocNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Docnet")]
-[assembly: AssemblyCopyright("Copyright ©2019 Frans Bouma")]
+[assembly: AssemblyCopyright("Copyright ©2020 Frans Bouma")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.4.0")]
-[assembly: AssemblyFileVersion("0.16.4")]
+[assembly: AssemblyVersion("0.16.5.0")]
+[assembly: AssemblyFileVersion("0.16.5")]
